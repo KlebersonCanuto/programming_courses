f97d4936b64c379ace767dfd37c2dd912312e734
Kleberson Canuto <klebersoncanuto@gmail.com>
2022-02-06 22:33:42 -0300

    Adição de testes de shell

---
+++ api/__tests__/utils/shell.test.js
@@ -0,0 +1,52 @@
+const shell = require('../../utils/shell');
+
+const code = 'x = input()\ny = input()\nprint(x + y)';
+
+describe('Test Shell', () => {  
+
+	it('Should exec shell', async () => {
+        const output = await shell.execShell(code, [{input: 'a\nb'}]);
+		expect(output[0].output).toEqual('ab');
+	});
+
+	it('Should fail to exec shell', async () => {
+		await expect(shell.execShell(code, [{input: 'a'}])).rejects.toThrow();
+	});
+
+	it('Should compare correct', async () => {
+        const correct = await shell.compare(code, [{input: 'a\nb', output: 'ab'}]);
+		expect(correct).toEqual(true);
+	});
+
+	it('Should compare incorrect', async () => {
+        const correct = await shell.compare(code, [{input: '1\n2', output: '3'}]);
+		expect(correct).toEqual(false);
+	});
+
+	it('Should fail to compare', async () => {
+		await expect(shell.compare(code, [{input: 'a', output: 'a'}])).rejects.toThrow();
+	});
+
+	it('Should compareIO correct', async () => {
+        const correct = await shell.compareIO(code, 'a\nb', 'ab');
+		expect(correct).toEqual(true);
+	});
+
+	it('Should compareIO incorrect', async () => {
+        const correct = await shell.compareIO(code, 'a\nb', 'ba');
+		expect(correct).toEqual(false);
+	});
+
+	it('Should fail to compareIO', async () => {
+		await expect(shell.compareIO(code, 'a', 'ab')).rejects.toThrow();
+	});
+
+	it('Should get output', async () => {
+        const output = await shell.getOutput(code, 'a\nb');
+		expect(output).toEqual('ab');
+	});
+
+	it('Should fail to get output', async () => {
+		await expect(shell.getOutput(code, 'a')).rejects.toThrow();
+	});
+});
