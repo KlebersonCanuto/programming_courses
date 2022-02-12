8a460e4d400c6cc65ceb48e297ef69656870f53a
Kleberson Canuto <klebersoncanuto@gmail.com>
2022-02-11 22:50:25 -0300

    Conclusão de testes de module service

--- api/__tests__/service/moduleService.test.js
+++ api/__tests__/service/moduleService.test.js
@@ -1,5 +1,5 @@
 const moduleService = require('../../service/moduleService');
-const { Course, Module } = require('../../database/models');
+const { Course, Module, Quiz, Problem, Material, User } = require('../../database/models');
 
 generateResponse = (fn) => {
 	const response = {
@@ -14,7 +14,11 @@ generateResponse = (fn) => {
 };
 
 describe('Test Module', () => {  
+	let quizId;
+	let problemId;
+	let materialId;
 	let courseId;
+	let userId;
 	let id;
 
 	beforeAll(async () => {
@@ -84,6 +88,104 @@ describe('Test Module', () => {
 		}, res);
 	});
 
+    it('Should get by id with progress (nothing registered)', async () => {
+        const user = await User.create({
+            username: 'User',
+            password: "User",
+            email: 'email@email.com',
+        });
+        userId = user.id;
+
+        const res = generateResponse((response) => {
+            expect(res.code).toEqual(200);
+            expect(response.data.name).toEqual('Module2');
+            expect(response.data.progressMaterials).toEqual(1);
+            expect(response.data.progressProblems).toEqual(1);
+            expect(response.data.progressQuizzes).toEqual(1);
+        });
+
+		await moduleService.getUser({
+            params: {
+                id,
+                userId
+            }
+		}, res);
+	});
+
+    it('Should get by id with valid user data', async () => {
+        const material = await Material.create({
+            title: 'Material',
+			content: '123', 
+			complementary: false,
+			number: 0,
+			video_link: '',
+			ModuleId: id,
+        });
+        materialId = material.id;
+
+        const problem = await Problem.create({
+			title: 'Problem',
+			description: '123', 
+			ModuleId: id,
+            file_id: '123'
+        });
+        problemId = problem.id;
+
+        const quiz = await Quiz.create({
+			title: 'Quiz',
+			question: '123', 
+			hint: '123', 
+			answers: ["123", "1", "2", "3"],
+			number: 0,
+			ModuleId: id,
+        });
+        quizId = quiz.id;
+       
+
+        const res = generateResponse((response) => {
+            expect(res.code).toEqual(200);
+            expect(response.data.name).toEqual('Module2');
+            expect(response.data.progressMaterials).toEqual(0);
+            expect(response.data.progressProblems).toEqual(0);
+            expect(response.data.progressQuizzes).toEqual(0);
+        });
+
+		await moduleService.getUser({
+            params: {
+                id,
+                userId: userId
+            }
+		}, res);
+	});
+
+    it('Should fail to get module', async () => {
+        const spy = jest.spyOn(Module, 'findByPk').mockImplementation(() => { throw new Error('error'); });
+        const res = generateResponse((response) => {
+            expect(res.code).toEqual(400);
+        });
+
+		await moduleService.get({
+            params: {
+                id
+            }
+		}, res);
+		spy.mockRestore();
+	});
+
+    it('Should fail to get module with user data', async () => {
+        const spy = jest.spyOn(Module, 'findByPk').mockImplementation(() => { throw new Error('error'); });
+        const res = generateResponse((response) => {
+            expect(res.code).toEqual(400);
+        });
+
+		await moduleService.getUser({
+            params: {
+                id
+            }
+		}, res);
+		spy.mockRestore();
+	});
+
 	it('Should failt to create module (invalid course)', async () => {
         const res = generateResponse((response) => {
             expect(res.code).toEqual(400);
@@ -120,6 +222,20 @@ describe('Test Module', () => {
                 id
             }
 		}, res);
+	});
+
+    it('Should fail to remove module', async () => {
+        const spy = jest.spyOn(Module, 'destroy').mockImplementation(() => { throw new Error('error'); });
+        const res = generateResponse((response) => {
+            expect(res.code).toEqual(400);
+        });
+
+		await moduleService.remove({
+            params: {
+                id
+            }
+		}, res);
+		spy.mockRestore();
 	});
 
     it('Should remove module', async () => {
