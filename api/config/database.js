48ee05c1a3f3d53700ebcfa0fdb5fa1d53033fea
Kleberson Canuto <klebersoncanuto@gmail.com>
2022-02-13 12:50:05 -0300

    Adição de docker e correções

--- api/config/database.js
+++ api/config/database.js
@@ -4,8 +4,19 @@ const database = {
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
-	database: process.env.NODE_ENV === 'test' ? process.env.DB_TEST_NAME : process.env.DB_NAME,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT,
 	dialect: 'mysql',
 };
+
+if (process.env.NODE_ENV === 'test') {
+	database.database = process.env.DB_TEST_NAME;
+}
+
+if (process.env.NODE_ENV === 'production') {
+	database.dialectOptions = {
+		ssl:'Amazon RDS'
+	};
+}
 
 module.exports = database;
